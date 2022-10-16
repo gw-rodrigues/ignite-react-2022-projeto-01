ea9029b2d6b38cdce1f5d7afc326c690cc9c2cb1
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-16 13:26:13 +0200

    feat: creating post and post style component

---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css";
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/gw-rodrigues.png" />
+          <div className={styles.authorInfo}>
+            <strong>Gleydson W. Rodrigues</strong>
+            <span>Dev Front-end</span>
+          </div>
+        </div>
+
+        <time
+          title="16 de Outubro 2022 às 12:00h"
+          dateTime="2022-10-16 12:00:00"
+        >
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Fala galeraa 👋 </p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
