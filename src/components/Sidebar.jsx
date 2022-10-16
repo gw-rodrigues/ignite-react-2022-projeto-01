3c85dbf1d0e668c88138fc0cc03c0f7992ed40eb
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-16 12:11:50 +0200

    ADD sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Pencil } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -8,11 +9,19 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
       />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/gw-rodrigues.png"
+        />
+
         <strong>Gleydson W. Rodrigues</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <Pencil size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
