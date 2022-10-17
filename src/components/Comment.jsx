783ad907f690bf6171ebf2360d5ac940730a33bb
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-17 11:34:00 +0200

    feat: add avatar component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,11 @@
 import { Trash, ThumbsUp } from "phosphor-react";
+import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/gw-rodrigues.png" />
+      <Avatar hasBorder={false} src="https://github.com/gw-rodrigues.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
