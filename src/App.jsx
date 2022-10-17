0b32583078855db7e15bb880d2d20512d80ba4c0
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-17 16:12:31 +0200

    feat: add post values sending to props

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,64 @@ import { Post } from "./components/Post";
  *
  * podemos usar método destructing {author, content} = props
  */
+
+/**
+ * author: { avatarUrl:"", name:"", role:"" }
+ * publishedAt: Date
+ * content: String
+ */
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/gw-rodrigues.png",
+      name: "Gleydson W. Rodrigues",
+      role: "Front-end Dev",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+      { type: "tag", content: "#novoprojeto" },
+      { type: "tag", content: "#nlw" },
+      { type: "tag", content: "#rocketseat" },
+    ],
+    publishedAt: new Date("2022-10-17 15:15:00"),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educador @Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+      { type: "tag", content: "#novoprojeto" },
+      { type: "tag", content: "#nlw" },
+      { type: "tag", content: "#rocketseat" },
+    ],
+    publishedAt: new Date("2022-09-15 15:15:00"),
+  },
+];
+
 function App() {
   return (
     <>
@@ -28,8 +86,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => (
+            <Post
+              key={post.id}
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </>
