634c16febfebc74daf4eaaecbc8f12057e5f692d
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-10-17 17:22:42 +0200

    feat: add key values to map arrays

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -75,11 +75,11 @@ export function Post({ author, content, publishedAt }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           }
           if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -88,7 +88,11 @@ export function Post({ author, content, publishedAt }) {
         <p>
           {content.map((line) => {
             if (line.type === "tag") {
-              return <a href="#">{line.content}</a>;
+              return (
+                <a key={line.content} href="#">
+                  {line.content}
+                </a>
+              );
             }
           })}
         </p>
@@ -110,7 +114,7 @@ export function Post({ author, content, publishedAt }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => (
-          <Comment content={comment} />
+          <Comment key={comment} content={comment} />
         ))}
       </div>
     </article>
